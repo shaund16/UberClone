24eb62b98f0efc80a23db433704923cbf4271196
Shaun <s-purslow@hotmail.com>
2021-12-31 14:00:30 -0700

    wrap safe area provider, navigationContainer and provider in screen components

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Lets build UBER!</Text>
-        <StatusBar style='auto' />
-      </View>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <HomeScreen />
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
